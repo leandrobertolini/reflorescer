88ec58f89694b100392c5b39d7a4e526c758a4f5
Leandro Bertolini <lemos.bertolini@gmail.com>
2016-08-23 17:21:18 -0300

    Creating models:

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,49 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+
+    SHIRT_SIZES = (
+        ('P', 'Pequeno'),
+        ('M', 'Medio'),
+        ('G', 'Grande'),
+        ('GG', 'Extra Grande'),
+    )
+
+    SHOES = (
+    	('33','33'),
+		('34','34'),
+		('35','35'),
+		('36','36'),
+		('37','37'),
+		('38','38'),
+		('39','39'),
+		('40','40'),
+		('41','41'),
+		('42','42'),
+		('43','43'),
+		('44','44'),
+		('45','45'),
+    )
+
+    PGTO = (
+    	('Total','Total'),
+    	('Carteirinha','Carteirinha'),
+    	('Fantasia','Fantasia'),
+    )
+
+
+
+    responsavel = models.ForeignKey('auth.User')
+    nome 		= models.CharField(max_length=400)
+    email		= models.CharField(max_length=300)
+    cidade 		= models.CharField(max_length=200)
+    manequim 	= models.CharField(max_length=2, choices=SHIRT_SIZES)
+    calcado 	= models.CharField(max_length=2, choices=SHOES)
+    pagamento 	= models.CharField(max_length=20,choices=PGTO)
+    obs 		= models.TextField()
+
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -16,4 +55,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.nome
