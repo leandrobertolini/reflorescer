d3c27dd9ad3984c6db3af0728d56673ec7b271ac
Leandro Bertolini <leandro.bertolini@Security-MacBook-Leandro.local>
2016-09-12 15:49:48 -0300

    Refactoring choices

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.core.files.storage import FileSystemStorage
+from .choices import SHIRT_SIZES, SHOES, PGTO, CARTEIRINHA
 
 
 # Create your models here.
@@ -13,41 +14,6 @@ class Alas(models.Model):
 class Componentes(models.Model):
 
     fs = FileSystemStorage(location='blog/static/fotos/')
-
-    SHIRT_SIZES = (
-        ('P', 'Pequeno'),
-        ('M', 'Medio'),
-        ('G', 'Grande'),
-        ('GG', 'Extra Grande'),
-    )
-
-    SHOES = (
-        ('33', '33'),
-        ('34', '34'),
-        ('35', '35'),
-        ('36', '36'),
-        ('37', '37'),
-        ('38', '38'),
-        ('39', '39'),
-        ('40', '40'),
-        ('41', '41'),
-        ('42', '42'),
-        ('43', '43'),
-        ('44', '44'),
-        ('45', '45'),
-    )
-
-    PGTO = (
-        ('Em Aberto', 'Em Aberto'),
-        ('Total', 'Total'),
-        ('Carteirinha', 'Carteirinha'),
-        ('Fantasia', 'Fantasia'),
-    )
-
-    CARTEIRINHA = {
-        ('Pendente','Pendente'),
-        ('Entregue','Entregue'),
-    }
 
     responsavel = models.ForeignKey('auth.User')
     nome = models.CharField(max_length=400)
