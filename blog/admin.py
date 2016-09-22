6752b8c2b120142727d8a370361838d316d11ef5
Leandro Bertolini <leandro.bertolini@Security-MacBook-Leandro.local>
2016-09-22 19:05:05 -0300

    Add query filter on componentes

--- blog/admin.py
+++ blog/admin.py
@@ -6,14 +6,14 @@ from .models.ala import Ala
 from .models.diretoria import Diretoria
 
 class DiretoriaAdmin(admin.ModelAdmin):
-	list_filter = ('ala',)
-	#search_fields = ('diretor',)
-	list_display = ('diretor','ala')
+    list_filter = ('ala',)
+    #search_fields = ('diretor',)
+    list_display = ('diretor','ala')
 
 
 class AlasAdmin(admin.ModelAdmin):
     search_fields = ('nome',)
-    list_display = ('nome', 'total_componentes','logo_ala')	
+    list_display = ('nome', 'total_componentes','logo_ala') 
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -26,6 +26,11 @@ class PostAdmin(admin.ModelAdmin):
         obj.responsavel = request.user
         obj.save()
 
+    def get_queryset(self, request):
+#        import ipdb
+#        ipdb.set_trace()
+        diretoria = Diretoria.objects.filter(diretor=request.user)
+        return Componente.objects.filter(ala_id__in = [x.ala_id for x in diretoria.all()]).all()
 
 # Register your models here.
 admin.site.register(Componente, PostAdmin)
